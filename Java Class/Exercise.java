699be6c46dafe23ac80e3ebfe014b70342f2a3a9
utujesandrine456 <utujesandrine456@gmail.com>
2025-09-16 11:01:41 +0200

    Add Java Class materials

---
+++ Java Class/Exercise.java
@@ -0,0 +1,234 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class Exercise {
+    public static void main(String[] args){
+        // System.out.print("Numbers from 1 to 10: \n");
+        // for(int i = 1; i <= 10; i++){
+        //     System.out.println(i);
+        // }
+
+        
+        // int number = 1;
+        // while( number <= 20){
+        //     if(number % 2 == 0){
+        //         System.out.println(number);
+        //     }
+        //     number ++;
+        // }
+
+
+        // int sum = 0 ;
+        // for(int i = 1; i <= 10; i++){
+        //     sum += i;
+        // }
+        // System.out.println("The sum of 100 natural numbers is: " + sum);
+
+
+        // int arr[] = {1,2,3,4,5};
+        // System.out.println("The reversed array is:  ");
+        // for(int i =4; i >= 0; i--){
+        //     System.out.println(arr[i]);
+        // }
+
+
+        // int arr[] = {10, 20, 212, 23 , 57, 134, 675};
+        // int max = arr[0];
+        // int size = 7;
+
+        // for(int i = 0; i < size; i++){
+        //     if(arr[i] > max){
+        //         max = arr[i];
+        //     }
+        // }
+
+        // System.out.println("The maximum number in the array is: " + max);
+
+
+
+        // int arr[] = {10, 20, 212, 23 , 57, 134, 675};
+        // int counterofodd = 0;
+        // int counterofeven = 0;
+
+        // for(int i = 0; i < 7; i++){
+        //     if(arr[i] % 2 == 0){
+        //         counterofeven ++;
+        //     }else{
+        //         counterofodd ++;
+        //     }
+        // }
+
+        // System.out.println("The number of Odd numbers are: " + counterofodd);
+        // System.out.println("The number of Even numbers are: " + counterofeven);
+
+
+
+        // int arr[] = {10, 20, 212, 23 , 57, 134, 675};
+        // int element = 20;
+
+        // for(int i = 0; i < 7; i++){
+        //     if(arr[i] == element){
+        //         System.out.println("Element Found ");
+        //         return ;
+        //     }
+        // }
+        // System.out.println("Element not found");
+
+
+
+        // int number = 5;
+        // System.out.println("The multiplication Table of 5: \n");
+        // for(int i = 1 ; i <= 10; i ++){
+        //     System.out.println(number * i);
+        // }
+
+
+        
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter any integer: ");
+        // int number = scanner.nextInt();
+        // scanner.close();
+
+        // int factorial = 1;
+        // for(int i = 1; i <= number ; i++){
+        //     factorial *= i;
+        // }
+        // System.out.println("The factorial of " + number + "is: " + factorial); 
+        
+        
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter any integer: ");
+        // int number = scanner.nextInt();
+        // scanner.close();
+
+        // int counter = 0;
+        // for(int a = 2; a < number; a++){
+        //     if(number % a == 0){
+        //         counter = 1;
+        //     }
+        // }
+
+        // if(counter == 0){
+        //     System.out.println("This is a prime number");
+        // }else{
+        //     System.out.println("The number is not a prime number");
+        // }
+
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter day of Week: ");
+        // int day = scanner.nextInt();
+        // scanner.close();
+
+        // switch (day) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Satarday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+
+        //     default:
+        //         System.out.println("This day does not exist");
+        //         break;
+
+        // }
+
+
+
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter vowel or consonant: ");
+        // String string = scanner.next();
+        // scanner.close();
+
+        // switch (string) {
+        //     case "a":
+        //         System.out.println("This is a vowel");
+        //         break;
+        //     case "i":
+        //         System.out.println("This is a vowel");
+        //         break;
+        //     case "u":
+        //         System.out.println("This is a vowel");
+        //         break;
+        //     case "e":
+        //         System.out.println("This is a vowel");
+        //         break;
+        //     case "o":
+        //         System.out.println("This is a vowel");
+        //         break;
+            
+        //     default:
+        //         System.out.println("This is a consonant");
+        //         break;
+        // }
+
+
+
+        // int arr[] = {10, 20, 212, 23 , 57, 134, 675};
+        // int sum = 0;
+        // int average = 0;
+
+        // for(int i = 0; i<7; i++){
+        //     sum += arr[i];
+        // }
+        // average = sum / 7;
+        // System.out.println("The average of elements in array: " + average);
+
+
+        // int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+        // float avg, sum = 0;
+        // int length = ages.length;
+
+        // for(int age: ages){
+        //     sum += age;
+        // }
+
+        // avg = sum / length;
+        // System.out.println("The average age is : " + avg);
+
+
+
+        // String[] cars={"Sandrine", "Baraka", "Slanie", "Pacifique", "Liberee", "Appolinaire"};
+        // Arrays.sort(cars);
+        // for(String car: cars){
+        //     System.out.print(car);
+        //     System.out.println("\n");
+        // }
+
+        int number = 10;
+
+        for(int i = 0; i <= number; i++){
+            System.out.println(Fibonacci(i));
+        }
+    }
+
+
+    static int Fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }else if(n == 1){
+            return 1;
+        }else{
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
